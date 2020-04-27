0a7aba594c8002ab7e6945d52dbc0cc3438dd938
Paul Nel <paul.nel@aurecongroup.com>
2020-04-27 13:36:42 +0200

    Updates

--- 03_poly/main_game.py
+++ 03_poly/main_game.py
@@ -6,6 +6,7 @@ from server import Server
 import random
 
 bond = Game()
+
 escape_car = Car(make="VW", year_manufactured=2017, engine="Small", colour="Blue")
 escape_boat = Boat(make="Regal", year_manufactured=1997, engine="Small", colour="Black")
 game_server = Server(name="Game Server", address="127.0.0.1", make="Dell", year_manufactured=2019)
